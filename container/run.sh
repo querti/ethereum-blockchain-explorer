6ffb28bfa5b68ffd98e710666b21736cc83e75c3
Lubomir Gallovic <lgallovi@localhost.localdomain>
2019-04-11 21:45:17 +0200

    asd

--- container/run.sh
+++ container/run.sh
@@ -3,7 +3,7 @@ SIZE=10000
 cd /home/ethereum-blockchain-explorer
 while true
 do
-    python3 main.py --interface /home/ethereum/geth.ipc --dbpath /home/db --gather_tokens True --bulk_size $SIZE --datapath /home/data 2 > /home/data/errlog
+    python3 main.py --interface /home/ethereum/geth.ipc --dbpath /home/db --gather_tokens True --bulk_size $SIZE --datapath /home/data 2>/home/data/errlog
     if [ "$?" == 1 ]
     then
         ERROR=`cat /home/data/errlog`
